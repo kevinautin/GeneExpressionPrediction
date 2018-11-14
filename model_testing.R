cb44374ccae3d6b52fa19d9a3f797596499fa9a8
kevinautin <34960189+kevinautin@users.noreply.github.com>
2018-11-15 00:39:19 +0900

    Add files via upload

--- model_testing.R
+++ model_testing.R
@@ -23,4 +23,17 @@ testing(method, folds, tune_grid, sequence,  targ, regu, preds_file, true_file)
 test_error(preds_file, true_file)
 stats = read.table("stats.txt")
 summary(stats)
+
+
+#Elastic net
+tune_grid = expand.grid(alpha=10^seq(0,-3, length=5),
+				lambda=10^seq(0,-3, length=5))
+preds_file = "preds_elanet.txt"
+true_file = "true_elanet.txt"
+method = "glmnet"
+sequence = seq(1, 1000)
+testing(method, folds, tune_grid, sequence,  targ, regu, preds_file, true_file)
+test_error(preds_file, true_file)
+stats = read.table("stats.txt")
+summary(stats)
 compareModels(preds_file, true_file, "preds_lasso.txt", "true_lasso.txt")
